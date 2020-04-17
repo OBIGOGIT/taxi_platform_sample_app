cbb249a7f682586cd06aee0c997b6adf1ba67bb4
taekyeongwon <qwesc1234@naver.com>
2020-04-17 16:43:42 +0900

    소스코드 apache 2.0 license 표기

--- app/src/main/java/com/example/aidltestclient/MainActivity.java
+++ app/src/main/java/com/example/aidltestclient/MainActivity.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2020 obigo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.aidltestclient;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,9 +31,6 @@ import com.obigo.nativelib.NCommunicator;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    private Button btn_aidl;
-    private Button btn_subscribe;
-    private Button btn_unsubcribe;
 
     private boolean isConnected = false;
     private Vehicle mVehicle;
@@ -32,16 +44,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setConnectionCallback();
 
-        btn_aidl = findViewById(R.id.btn_aidl);
-        btn_subscribe = (Button) findViewById(R.id.btn_subscribe);
-        btn_unsubcribe = findViewById(R.id.btn_unsubscribe);
-
-        btn_aidl.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
+        Button btn_subscribe = (Button) findViewById(R.id.btn_subscribe);
+        Button btn_unsubcribe = (Button) findViewById(R.id.btn_unsubscribe);
 
         btn_subscribe.setOnClickListener(new View.OnClickListener() {
             @Override
